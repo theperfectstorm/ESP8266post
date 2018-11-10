4b0fff1d76189c62cf7e4cce532b2f01afceb135
theperfectstorm <abhishek.singhgtt@gmail.com>
2018-11-10 10:09:37 +0530

    Update monitor.php

--- monitor.php
+++ monitor.php
@@ -1,3 +1,17 @@
-<?php
-echo readfile("1.txt");
-?>
+<html>
+    <head>
+        <title>Air Quality Manager</title>
+    </head>
+    <body>
+        <?php
+        $myfile = fopen("1.txt", "r") or die("Unable to open file");
+        $val= fread($myfile,filesize("1.txt"));
+        fclose($myfile);
+        echo "<h2>PPM Value : $val </h2><br/>";
+        ?>
+        
+        
+        
+        
+    </body>
+</html>
