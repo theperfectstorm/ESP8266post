8159d9167ad23f04be5071b4690025232f239e9a
theperfectstorm <abhishek.singhgtt@gmail.com>
2018-11-13 00:53:33 +0530

    Update monitor.php

--- monitor.php
+++ monitor.php
@@ -8,9 +8,20 @@
         $val= fread($myfile,filesize("1.txt"));
         fclose($myfile);
         echo "<h2>PPM Value : $val </h2><br/>";
+        
+        
+        $n=(int)val;
+        if($n>200)
+        {
+            echo "<br/> Air quality is poor";
+        }
+        else
+        echo "<br/> Air quality is good";
+        $url1=$_SERVER['REQUEST_URI'];
+    header("Refresh: 5; URL=$url1");
+        
         ?>
-        
-        
+         
         
         
     </body>
